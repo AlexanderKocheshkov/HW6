486eb23f680fc06e01443ab37aafb47ed4a56769
AlexanderKocheshkov <99826379+AlexanderKocheshkov@users.noreply.github.com>
2022-12-19 22:15:05 +0300

    Add files via upload

---
+++ KocheshkovAK_HW6_part3.py
@@ -0,0 +1,14 @@
+def xor_encrypt( file_text, key ):
+    encrypt_str = ""
+    for line in file_text:
+        for letter in line:
+            encrypt_str += chr( ord(letter) ^ key )
+    return  encrypt_str
+f = open("File.txt", "r") 
+text = f.read()
+key  = 8
+print( "Оригинал:\t", text )
+encr_strg = xor_encrypt( text, key ) 
+print( "Зашифрованное:\t", encr_strg )
+print( "Расщифрованное:\t", xor_encrypt( encr_strg, key ) )
+f.close()
